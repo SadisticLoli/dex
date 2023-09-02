4127345e69d8cd19f7fdc4cb86e5b3678f5101b1
Loli <46930958+SadisticLoli@users.noreply.github.com>
2023-09-02 14:40:53 +0300

    Update dexnewest.lua

--- dexnewest.lua
+++ dexnewest.lua
@@ -1,4 +1,4 @@
-getfenv(1).currentTheme = "Graphite" -- Change to an valid theme(example: "White")
+-- getfenv(1).currentTheme = "Graphite" -- Change to an valid theme(example: "White")
 -- Themes: Graphite, Platinum, White, Red, Cyan, Yellow, Orange
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/loglizzy/dex-custom-icons/main/main.lua"))()
 
